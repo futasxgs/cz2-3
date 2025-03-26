43f03eacad4c562861e29dce9235a15814f26657
futasxgs <s33295@pjwstk.edu.pl>
2025-03-26 20:04:58 +0200

    Delete 1 directory

--- 1/Новый текстовый документ.java
+++
@@ -1,8 +0,0 @@
-import java.until.Scanner;
-public class Новый текстовый документ {
-
-    public static void main(String[] args)
-    Scanner sc = new Scanner(System.in);
-    System.out.print("write farengates:");
-    
-}
